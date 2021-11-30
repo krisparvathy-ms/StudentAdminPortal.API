ac55f948db13e2a5eb47884f2f9d05245b22f1d6
parvathyms98 <krisparvathyms@gmail.com>
2021-11-30 10:11:35 +0530

    End of Section 2-API

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/DomainModels/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace StudentAdminPortal.API.DomainModels
+{
+    
+        public class Gender
+        {
+            public Guid Id { get; set; }
+            public string Description { get; set; }
+        }
+    
+}
---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Repositories/SqlStudentRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAdminPortal.API.DataModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Repositories
+{
+    public class SqlStudentRepository : IStudentRepository
+    {
+        private readonly StudentAdminContext context; //throughout this SqlStudentRepo we'll use this context to talk to our databse
+
+        public SqlStudentRepository(StudentAdminContext context)
+        {
+            this.context = context;
+        }
+        public async Task<List<Student>> GetStudentsAsync()
+        {
+            // return context.Student.ToList(); //linq query
+            return await context.Student.Include(nameof(Gender)).Include(nameof(Address)).ToListAsync();
+        }
+
+    }
+}
